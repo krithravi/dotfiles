8a1d22de85f32f9cd35109473b881ecfe3551454
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2022-08-09 10:32:11 -0500

    locking prettier

---
+++ .config/i3/lock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+i3lock -i /home/krithika/Pictures/neg_doordurin.png -t -e -k \
+--insidever-color=#3baa9c --ringver-color=#126d61 --verif-text="Verifying..." \
+--insidewrong-color=#dd0687 --ringwrong-color=#99065e --wrong-text="Wrong ☹" \
+--time-color=#64db98 --date-color=#64db98 --date-str="%A %b %d" --noinput-text="" --radius=120 --indicator --ring-width=11 --bshl-color=#311572 --ring-color=#64db98 --keyhl-color=#237762
