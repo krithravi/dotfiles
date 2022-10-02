0df2f1cc7858e8194626cb994a81dd0fbe5f56cd
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2022-10-02 14:27:48 -0500

    better i3 coloring

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -2,5 +2,5 @@
 
 i3lock -i /home/krithika/Pictures/neg_doordurin.png -t -e -k \
 --insidever-color=#3baa9c --ringver-color=#126d61 --verif-text="Verifying..." \
---insidewrong-color=#ea839b --ringwrong-color=#99065e --wrong-text="Wrong ☹" \
+--insidewrong-color=#c3c3c3 --ringwrong-color=#99065e --wrong-text="Wrong ☹" \
 --time-color=#64db98 --date-color=#64db98 --date-str="%A %b %d" --noinput-text="Empty" --radius=120 --indicator --ring-width=11 --bshl-color=#311572 --ring-color=#64db98 --keyhl-color=#237762
