a1f78b33609f99a992356b33689d7732c3bc3120
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2023-05-21 19:49:06 -0500

    updated desktops

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i3lock -i /home/krithika/Pictures/neg_doordurin.png -t -e -k \
+i3lock -i /home/krithika/dotfiles/desktops/neg_doordurin.png -t -e -k \
 --insidever-color=#3baa9c --ringver-color=#126d61 --verif-text="Verifying..." \
 --insidewrong-color=#c3c3c3 --ringwrong-color=#99065e --wrong-text="Wrong ☹" \
 --time-color=#64db98 --date-color=#64db98 --date-str="%A %b %d" --noinput-text="Empty" --radius=120 --indicator --ring-width=11 --bshl-color=#311572 --ring-color=#64db98 --keyhl-color=#237762
