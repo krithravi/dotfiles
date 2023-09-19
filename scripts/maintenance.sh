ea155e578198ebcb937517ab6867650df18f0d7e
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2023-09-18 23:32:18 -0500

    fix to script

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -3,6 +3,6 @@ FILENAME=~/log.txt
 
 echo `date '+%a %d %b %Y %T %Z'` >> $FILENAME
 echo '    started' >> $FILENAME
-sudo pacman -Syu
-sudo pacman -Qdtq | sudo pacman -Rs -
+sudo pacman -Syu --noconfirm
+sudo pacman -Qdtq --noconfirm | sudo pacman -Rs --noconfirm -
 echo '    exited' >> $FILENAME
